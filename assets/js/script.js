a148b3f7c4783ef1238b8ca361c8b2d797881f66
DanielLiszka <liszkadp@alumni.vcu.edu>
2022-03-20 21:28:44 -0400

    added input options

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,36 @@ var uppercaseCharcters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberCharcters = "0123456789";
 var specialCharcters = "~}|{`_^]\[@?>=<;:/.-,+*)('&%$#!";
 
+function input(){
+    passwordLength = Number(prompt("Please choose a password length between 8 and 128"));
+    while (passwordLength === null){
+      alert("a response is needed");
+      passwordLength = Number(prompt("Please choose a password length between 8 and 128"));}
+    
+    while (passwordLength < 8){
+      alert("password length must be at least 8 numbers");
+      passwordLength = Number(prompt("Please choose a password length between 8 and 128"));}
+
+    while (passwordLength > 128){
+      alert("password length must be no more than 128 numbers");
+      passwordLength = Number(prompt("Please choose a password length between 8 and 128"));}
+
+    inputNumber = confirm("should it contain numbers?");
+    inputSpecialCharcters = confirm("should it contain special charcters?")
+    inputUppercase = confirm("should it contain uppercase letters?")
+    inputLowercase = confirm("should it contain lowercase letters?")
+     
+
+}
+
+function RandomInteger(max_num) {
+  return Math.floor(Math.random()*max_num);
+}
+
+function generatePassword() {
+  var password_input = input()
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -12,9 +42,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
